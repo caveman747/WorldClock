6577072f364ab899722dd667e99b60a592899040
caveman747 <johnmredfield@gmail.com>
2022-09-21 11:05:39 -0700

    Good for testing on the big screen

--- main.py
+++ main.py
@@ -13,9 +13,7 @@ root = Tk()
 # root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(),
 #                                        root.winfo_screenheight()))
 
-mycolor = '#%02x%02x%02x' % (000, 37, 150)  # set your favourite rgb color
-
-root.configure(bg=mycolor)
+mycolor = '#%02x%02x%02x' % (2, 30, 110)  # set your favourite rgb color
 
 
 root.attributes("-fullscreen", True)
@@ -24,8 +22,8 @@ def times():
     home = pytz.timezone("America/Los_Angeles")
     local_time=datetime.now(home)
     datime = local_time.strftime('%H:%M:%S %p')
-    clock1.config(text=datime, bg=mycolor)
-    name1.config(text="Time in Eugene Oregon", bg=mycolor)
+    clock1.config(text=datime)
+    name1.config(text="Time in Eugene Oregon")
     clock1.after(200, times)
 
     # home = pytz.timezone("Europe/Rome")
@@ -46,18 +44,19 @@ def times():
     # clock4.config(text=datime)
     # name.config(text="Slovakia")
 
-name1 = Label(root, font=("calibri", 20, "bold"))
-name1.place(rely=.1, relx=.1)
-clock1=Label(root, font=("calibri", 25, "bold"))
-clock1.place(rely=.2, relx=.1)
+background_image=ImageTk.PhotoImage(Image.open("/home/tester/PycharmProjects/WorldClock /LockScreen-Eugene.png"))
+background_label = Label(root, image=background_image)
+background_label.pack(fill=tkinter.BOTH)
 
 path = "/home/tester/PycharmProjects/WorldClock /DL-2.jpg"
-
 img = ImageTk.PhotoImage(Image.open(path))
+panel = Label(root, image = img, borderwidth=0, highlightthickness=0)
+panel.place(rely=.4, relx=.12)
 
-panel = Label(root, image = img)
-
-panel.place(rely=.25, relx=.12)
+name1 = Label(root, fg= "white", bg=mycolor, font=("calibri", 20, "bold"))
+name1.place(rely=.1, relx=.8)
+clock1=Label(root, fg="white", bg=mycolor, font=("calibri", 25, "bold"))
+clock1.place(rely=.2, relx=.8)
 
 # name2 = Label(root, font=("times", 20, "bold"))
 # name2.place(rely=.1, relx=.1)
